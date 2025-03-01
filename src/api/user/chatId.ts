e6daaa291634997fdf3b5f47b6dc4a221948fda2
Swineherdd <sasha_ignatovskiy@mail.ru>
2025-03-01 13:07:55 +0300

    set

--- src/api/user/chatId.ts
+++ src/api/user/chatId.ts
@@ -1,8 +1,8 @@
 import WebApp from '@twa-dev/sdk'
 export const getChatId = async () => {
 	if (typeof window !== 'undefined') {
-		return 478689305
-		// return await WebApp.initDataUnsafe?.user?.id
+		
+		return await WebApp.initDataUnsafe?.user?.id
 	}
 	return 132
 }
